b07c2a786eda73aa989e6adbab657b03a3fbdffc
JamalMarques <yamamarques@gmail.com>
2015-03-24 18:41:45 -0300

    Including Lat y Long from Google Play Services

--- Application/src/main/java/com/example/android/wearable/watchface/ShowActivity.java
+++ Application/src/main/java/com/example/android/wearable/watchface/ShowActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -23,6 +24,7 @@ import com.example.android.wearable.watchface.weather.OpenWeatherListener;
 import com.example.android.wearable.watchface.weather.OpenWeatherRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.Node;
@@ -49,12 +51,16 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
     private RadioButton rBlack,rWhite;
     private Spinner styleSpinner, refreshSpinner;
 
-    private GoogleApiClient googleApiClient;
+    private GoogleApiClient googleApiClient, positionGoogleApiClient;
     private AlarmManager alarmManager;
     private SharedPreferences sharedPreferences;
 
     private SpiceManager spiceManager = new SpiceManager(SpiceService.class);
     private String lastRequestCacheKey;
+
+    private Location lastLocation;
+    private String lastLong;
+    private String lastLat;
 
     protected SpiceManager getSpiceManager()
     {
@@ -72,6 +78,12 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
                                                            .addConnectionCallbacks(this)
                                                            .addOnConnectionFailedListener(this)
                                                            .build();
+
+        positionGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
         alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME,0);
@@ -132,6 +144,7 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
     @Override
     protected void onStart() {
         super.onStart();
+        positionGoogleApiClient.connect();
         googleApiClient.connect();
         spiceManager.start(this);
     }
@@ -143,6 +156,9 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
         }
         if (spiceManager.isStarted()) {
             spiceManager.shouldStop();
+        }
+        if( positionGoogleApiClient != null && positionGoogleApiClient.isConnected()){
+            positionGoogleApiClient.disconnect();
         }
         super.onStop();
     }
@@ -157,7 +173,12 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                positionGoogleApiClient);
+        if (lastLocation != null) {
+            lastLat = String.valueOf(lastLocation.getLatitude());
+            lastLong = String.valueOf(lastLocation.getLongitude());
+        }
     }
 
     @Override
@@ -170,7 +191,7 @@ public class ShowActivity extends Activity implements GoogleApiClient.Connection
                 lastRequestCacheKey = request.createCacheKey();
                 spiceManager.execute(request, lastRequestCacheKey, DurationInMillis.ONE_MINUTE, new StockQuoteListener());
 
-                OpenWeatherRequest weatherRequest = new OpenWeatherRequest("-37.982593","-57.554475","metric");
+                OpenWeatherRequest weatherRequest = new OpenWeatherRequest(lastLat,lastLong,"metric");
                 lastRequestCacheKey = weatherRequest.createCacheKey();
                 spiceManager.execute(weatherRequest,lastRequestCacheKey,DurationInMillis.ONE_HOUR,new OpenWeatherListener(googleApiClient));
         }
